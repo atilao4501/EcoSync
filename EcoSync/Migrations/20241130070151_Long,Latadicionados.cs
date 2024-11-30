518d2f9f5ea09c1f3a89f98d741429ab88a596fe
atilaalcantara <atila.alcantara@gavresorts.com.br>
2024-11-30 04:13:35 -0300

    feat: long e lat adicionados

---
+++ EcoSync/Migrations/20241130070151_Long,Latadicionados.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class LongLatadicionados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
