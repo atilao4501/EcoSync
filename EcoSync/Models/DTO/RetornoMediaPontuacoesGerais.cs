c72dd671560e2f9ea3f89c335750230854dfb43b
atilaalcantara <atila.alcantara@gavresorts.com.br>
2024-11-30 12:59:09 -0300

    sync: migrations

---
+++ EcoSync/Models/DTO/RetornoMediaPontuacoesGerais.cs
@@ -0,0 +1,8 @@
+public class RetornoMediaPontuacoesGerais
+{
+    public double MediaPontuacaoAreaVerde { get; set; }
+    public double MediaPontuacaoEducacao { get; set; }
+    public double MediaPontuacaoSaude { get; set; }
+    public double MediaPontuacaoDensidadePopulacional { get; set; }
+    public double MediaPontuacaoTotal { get; set; }
+}
